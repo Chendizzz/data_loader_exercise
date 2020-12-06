f46b8da26b1cf5e5c77ebc9c597702fe7f4fb7c7
Chendizzz <chendi.zhu@rwth-aachen.de>
2020-12-06 20:25:12 +0100

    preprocessing of a dataset: paths and labels

--- image_folder_ex.py
+++ image_folder_ex.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
-
+import matplotlib as plt
 '''read the labels in the .csv file
 '''
 df = pd.read_csv('D:\\ML_data\\dog_breed\\labels.csv')
@@ -32,13 +32,13 @@ print(breed_120_list)
 dic = {}
 for i in range(len(breed_set)):
     dic[breed_120_list[i]] = i
-print(dic)
+# print(dic)
 
 file = Series.to_numpy(df['id'])
-print(file.shape)
+# print(file.shape)
 
 file_name = [i+'.jpg' for i in file]
-print(file_name)
+# print(file_name)
 
 file_with_path = [os.path.join('D:\\ML_data\\dog_breed\\origin_train_data', i) for i in file_name]
 
@@ -46,9 +46,63 @@ file_with_path = [os.path.join('D:\\ML_data\\dog_breed\\origin_train_data', i) f
 '''
 file_train_path = file_with_path[:8000]
 file_test_path = file_with_path[8000:]
-print(file_test_path)
+# print(file_test_path)
 
 '''save these paths with np.save(), as .npy file
 '''
 np.save('file_train_path.npy', file_train_path)
 np.save('file_test_path.npy', file_test_path)
+
+'''train labels
+'''
+number = []
+for i in range(10222):
+    number.append(  dic[ breed[i] ]  )
+number = np.array(number)
+number_train = number[:8000]
+number_test = number[8000:]
+np.save( "number_train.npy", number_train )
+np.save( "number_test.npy" , number_test )
+
+''' form a custom dataset
+'''
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+'''use pil to read images
+'''
+from PIL import Image
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+preprocessing = transforms.Compose([transforms.ToTensor(), normalize])
+
+
+def default_loader(path):
+    img_f = Image.open(path)
+    print('img_f type: ', type(img_f))
+    img_f = img_f.resize((224, 224))
+    img_tensor = preprocessing(img_f)
+    print(img_tensor.shape)
+    return img_tensor
+
+
+class trainset(Dataset):
+    def __init__(self, loader=default_loader):
+
+        self.images = file_train_path
+        self.target = number_train
+        self.loader = loader
+
+    def __getitem__(self, index):
+        fn = self.images[index]
+        img = self.loader(fn)
+        target = self.target[index]
+        return img, target
+
+    def __len__(self):
+        return len(self.images)
+
+train_data  = trainset()
+trainloader = DataLoader(train_data, batch_size=4, shuffle=True)
+
+for step, (batch_x, batch_y) in enumerate(trainloader):
+    print('step:', step, '| batch x:', batch_x.numpy(), '| batch_y:', batch_y.numpy())
